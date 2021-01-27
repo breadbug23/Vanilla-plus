6797186f77d1ca5d758fc6c04f4cfc1d2eb17278
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-01-27 23:27:10 +0100

    Update zapper.js

--- scripts/units/zapper.js
+++ scripts/units/zapper.js
@@ -1,8 +1,8 @@
 const zapper = extend(UnitType, "zapper", {
 	draw(unit){
 		this.super$draw(unit);
-		Draw.rect("vanilla-plus-zapper-orb", unit.x, unit.y, Time.time * 12);
-		Draw.rect("vanilla-plus-zapper-orb-outline", unit.x, unit.y, Time.time * 12);
+		Draw.rect("vanilla-+-zapper-orb", unit.x, unit.y, Time.time * 12);
+		Draw.rect("vanilla-+-zapper-orb-outline", unit.x, unit.y, Time.time * 12);
 	}
 });
 zapper.constructor = () => extend(UnitEntity, {});

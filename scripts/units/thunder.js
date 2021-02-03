aba10ba9c9c7f2304754909a3cfa4ed64bc0cd43
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-02-03 22:31:33 +0100

    Update thunder.js

--- scripts/units/thunder.js
+++ scripts/units/thunder.js
@@ -1,10 +1,10 @@
 const thunder = extend(UnitType, "thunder", {
 	draw(unit){
 		this.super$draw(unit);
-		Draw.rect("vanilla-plus-thunder-orb", unit.x, unit.y, Time.time * 12);
-		Draw.rect("vanilla-plus-thunder-orb", unit.x, unit.y, (Time.time + 3.75) * 12);
-		Draw.rect("vanilla-plus-thunder-orb-outline", unit.x, unit.y, Time.time * 12);
-		Draw.rect("vanilla-plus-thunder-orb-outline", unit.x, unit.y, (Time.time + 3.75) * 12);
+		Draw.rect("vanilla-+-thunder-orb", unit.x, unit.y, Time.time * 12);
+		Draw.rect("vanilla-+-thunder-orb", unit.x, unit.y, (Time.time + 3.75) * 12);
+		Draw.rect("vanilla-+-thunder-orb-outline", unit.x, unit.y, Time.time * 12);
+		Draw.rect("vanilla-+-thunder-orb-outline", unit.x, unit.y, (Time.time + 3.75) * 12);
 	}
 });
 thunder.constructor = () => extend(UnitEntity, {});

333b1c72701d97b7c2c17c4ad474977578f63281
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-02-03 22:32:29 +0100

    Update storm.js

--- scripts/units/storm.js
+++ scripts/units/storm.js
@@ -1,16 +1,16 @@
 const storm = extend(UnitType, "storm", {
 	draw(unit){
 		this.super$draw(unit);
-		Draw.rect("vanilla-plus-storm-orb", unit.x, unit.y, Time.time * 12);
-		Draw.rect("vanilla-plus-storm-orb", unit.x, unit.y, (Time.time + 6) * 12);
-		Draw.rect("vanilla-plus-storm-orb", unit.x, unit.y, (Time.time - 6) * 12);
-		Draw.rect("vanilla-plus-storm-orb", unit.x, unit.y, (Time.time + 12) * 12);
-		Draw.rect("vanilla-plus-storm-orb", unit.x, unit.y, (Time.time - 12) * 12);
-		Draw.rect("vanilla-plus-storm-orb-outline", unit.x, unit.y, Time.time * 12);
-		Draw.rect("vanilla-plus-storm-orb-outline", unit.x, unit.y, (Time.time + 6) * 12);
-		Draw.rect("vanilla-plus-storm-orb-outline", unit.x, unit.y, (Time.time - 6) * 12);
-		Draw.rect("vanilla-plus-storm-orb-outline", unit.x, unit.y, (Time.time + 12) * 12);
-		Draw.rect("vanilla-plus-storm-orb-outline", unit.x, unit.y, (Time.time - 12) * 12);
+		Draw.rect("vanilla-+-storm-orb", unit.x, unit.y, Time.time * 12);
+		Draw.rect("vanilla-+-storm-orb", unit.x, unit.y, (Time.time + 6) * 12);
+		Draw.rect("vanilla-+-storm-orb", unit.x, unit.y, (Time.time - 6) * 12);
+		Draw.rect("vanilla-+-storm-orb", unit.x, unit.y, (Time.time + 12) * 12);
+		Draw.rect("vanilla-+-storm-orb", unit.x, unit.y, (Time.time - 12) * 12);
+		Draw.rect("vanilla-+-storm-orb-outline", unit.x, unit.y, Time.time * 12);
+		Draw.rect("vanilla-+-storm-orb-outline", unit.x, unit.y, (Time.time + 6) * 12);
+		Draw.rect("vanilla-+-storm-orb-outline", unit.x, unit.y, (Time.time - 6) * 12);
+		Draw.rect("vanilla-+-storm-orb-outline", unit.x, unit.y, (Time.time + 12) * 12);
+		Draw.rect("vanilla-+-storm-orb-outline", unit.x, unit.y, (Time.time - 12) * 12);
 	}
 });
 storm.constructor = () => extend(UnitEntity, {});
